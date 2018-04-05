33f7413500516dae97375ab36bf4f8f3503fbae4
Asif Mohammad Mollah <asifmohammadmollah@gmail.com>
2018-04-05 13:08:30 +0530

    Update user fixed

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -6,7 +6,7 @@ class Api::AuthController < Api::BaseController
   def register
     @user=User.new(user_param)
     if @user.save
-      render json: {status: true, message: 'User registered !', user: @user}, status: 201
+      render json: {status: true, message: 'User registered !', user: {id: @user.id, email: @user.email, profile_attributes: @user.profile}}, status: 201
     else
       render json: {status: false, message: 'Failed to register !'}, status: 402
     end
@@ -20,9 +20,11 @@ class Api::AuthController < Api::BaseController
 
   def update_user
     @user=User.find_by_email(params[:user][:email])
-    @user.name=params[:user][:name]
+    @user.profile.name=params[:user][:profile_attributes][:name]
+    @user.profile.address=params[:user][:profile_attributes][:address]
+    @user.profile.age=params[:user][:profile_attributes][:age]
     if @user.save
-      render json: {status: true, message: 'User updated !', user: @user}, status: 200
+      render json: {status: true, message: 'User updated !', user: {id: @user.id, email: @user.email, profile_attributes: @user.profile}}, status: 200
     else
       render json: {status: false, message: 'Failed to update !'}, status: 402
     end
