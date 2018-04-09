003c11361586fe9e6c729b445a1f02d2b4e3de07
Asif Mohammad Mollah <asifmohammadmollah@gmail.com>
2018-04-10 01:11:26 +0530

    modified

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,28 +2,9 @@ class Api::BaseController < ActionController::Base
   # protect_from_forgery with: :exception
   attr_reader :current_user
 
-  def render_data(data, status)
-    render json: data, status: status, callback: params[:callback]
-  end
-
-  def render_error(message, code='internal_server_error', status = 400, data={})
-    payload = { success: false, status: status, code: code, message: message }
-    payload[:data] = data if data.present?
-    render_data(payload, status)
-  end
-
-  def render_success(data, status = 200)
-    if data.is_a? String
-      render_data({ message: data }, status)
-    else
-      render_data(data, status)
-    end
-  end
-
   protected
     # Validates the token and user and sets the @current_user scope
     def authenticate_request!
-      # binding.pry
       if !payload || !JWToken.valid_payload(payload.first)
         return invalid_authentication
       end
@@ -33,7 +14,7 @@ class Api::BaseController < ActionController::Base
 
     # Returns 401 response. To handle malformed / invalid requests.
     def invalid_authentication
-      render_error 'Authentication failure', 'unauthorized', 401
+      render json: {status: false, message: 'Authentication failure.'}, status: :unauthorized
     end
 
   private
@@ -48,6 +29,7 @@ class Api::BaseController < ActionController::Base
 
     # Sets the @current_user with the user_id from payload
     def load_current_user!
+      binding.pry
       @current_user = User.find_by(id: payload[0]['user_id'])
     end
 
